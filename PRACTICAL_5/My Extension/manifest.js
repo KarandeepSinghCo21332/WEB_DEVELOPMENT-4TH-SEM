dd65ca08b98ce12cf4d1a644d6f28c72234a81a7
Karandeep Singh <110185183+KarandeepSinghCo21332@users.noreply.github.com>
2023-04-08 02:10:37 +0530

    Create manifest.js

---
+++ PRACTICAL_5/My Extension/manifest.js
@@ -0,0 +1,14 @@
+{
+  "manifest_version": 2,
+  "name": "Web Page Downloader",
+  "version": "1.0",
+  "description": "Download the current web page as an HTML file.",
+  "permissions": [
+    "activeTab",
+    "downloads"
+  ],
+  "browser_action": {
+    "default_icon": "icon.png",
+    "default_popup": "popup.html"
+  }
+}
